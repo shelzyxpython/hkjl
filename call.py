244adc0d50086a500629c830df3290350f929e23
shelzyxpython <144206667+shelzyxpython@users.noreply.github.com>
2023-10-25 11:09:32 +0200

    Add files via upload

---
+++ call.py
@@ -0,0 +1,30 @@
+# s = int(input())
+# for i in range(1,s):
+#    if s % i == 0:
+#        print(i,end=" ")
+
+# def palindrom(a):
+#     return a == a[::-1]
+# s = input()
+# b = palindrom(s)
+# if b:
+#     print("Yes")
+# else:
+#     print("No")
+
+# def texts(text):
+#     words = text.split()
+#     word_1 = {}
+#     for word in words:
+#         if word in word_1:
+#             word_1[word] += 1
+#         else:
+#             word_1[word] = 1
+#     word_2 = max(word_1, key=word_1.get)
+#     longest_word = max(words, key=len)
+#     return word_2, longest_word
+# text = input()
+# most_frequent, longest = texts(text)
+# print(most_frequent)
+# print(longest)
+
